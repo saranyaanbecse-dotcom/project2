1f2512cbb9a7c8d419040f617bc0135bb949575b
saranyaanbecse-dotcom <saranyaanbecse@gmail.com>
2025-11-18 11:44:29 +0530

    art

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import ArticleBrowser from "./pages/ArticleBrowser";
-import ArticleDetail from "./pages/ArticleDetail";
+
 
 export default function App() {
   return (
     <Routes>
       <Route path="/articles" element={<ArticleBrowser />} />
-      <Route path="/article/:slug" element={<ArticleDetail />} />
+      <Route path="*" element={<Navigate to="/articles" />} />
+    </Routes>
+  );
+}export default function App() {
+  return (
+    <Routes>
+      <Route path="/articles" element={<ArticleBrowser />} />
+      <Route path="*" element={<Navigate to="/articles" />} />
     </Routes>
   );
 }
